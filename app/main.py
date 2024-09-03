2feb9ae7fcf7fb1cd42e8e47458507306913dfa5
Bong Bungalan <bbungalan@gmail.com>
2024-09-03 16:15:39 +0800

    wokring mongodb connection

--- app/main.py
+++ app/main.py
@@ -1,14 +1,19 @@
 from typing import Union
+from database.dbCfg import *
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
+    clusterPassword = "fastapipassword"
+    mongoClient = MongoDBClient(clusterPassword)
+    result = mongoClient.connect()
+    if isinstance(result, dict) and result.get("_isError"):
+        return result
+    else:
+        return "Hello WORLD!! Connected to MongoDB"
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
